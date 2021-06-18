a603bb13c45f0aab40c5b23e66f99f7ab39c84f4
darkness0710 <nguyenthanhhaid13cn7@gmail.com>
2021-06-18 16:12:37 +0700

    update

--- src/views/ShopDetail.vue
+++ src/views/ShopDetail.vue
@@ -95,7 +95,7 @@ export default {
             return formatter.format(cost).replace('$', '') + ' VNĐ';
         },
         formatTitle(item) {
-            let prefix = this.types[item.type][1];
+            let prefix = this.types.find(x => x[0] == item.type);
             return `${prefix} S ${item.server}`;
         },
         formatContent(item) {
@@ -109,15 +109,16 @@ export default {
     },
     created() {
         this.item = DataJson.data.find(x => x.id === this.$route.params.id);
+        
         let tn3q_types = ['0', '1'];
         let new_omg3q_types = ['2', '3', '4', '5'];
+
         if (tn3q_types.includes(this.item.type)) {
             this.types = TypeJson.tn3q;
         }
         if (new_omg3q_types.includes(this.item.type)) {
             this.types = TypeJson.new_omg3q;
         }
-        // this.types = TypeJson.data;
         this.slides = [];
         const illustrations = require.context(
           '@/assets/database/shop/',
